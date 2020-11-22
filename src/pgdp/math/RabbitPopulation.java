0ac14291360692d82cf5c054d30895dc7443cb4f
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-11-22 20:58:25 +0100

    22

--- src/pgdp/math/RabbitPopulation.java
+++ src/pgdp/math/RabbitPopulation.java
@@ -4,6 +4,24 @@ import static pgdp.MiniJava.*;
 
 public class RabbitPopulation {
 	public static void main(String[] args) {
+		int n = readInt("Bitte Zahl eingeben:");
 
+		int [] rabbits = new int [3];
+		rabbits [0] = 1;
+		int month = 1;
+
+		while (month < n){
+			int sum = 0;
+			int i = 0;
+			while (i < 3) {
+				sum += rabbits[i];
+				i++;
+			}
+			rabbits[2] = rabbits[1];
+			rabbits[1] = rabbits[0];
+			rabbits[0] = sum;
+			month++;
+		}
+		write(rabbits[0] + rabbits[1] + rabbits[2]);
 	}
 }
